f00db415cb8ae96f5b27829fb6503bbdd3a9ff79
juan velazquez <juanbello055@gmail.com>
2025-11-26 21:17:40 -0400

    error en archivo vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath, URL } from 'node:url'
 
-const CLOUDFLARE_TUNNEL_HOST = 'coral-eternal-weight-enclosure.trycloudflare.com';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,9 +10,6 @@ export default defineConfig({
   server:{
     host:true, //escucha todas las interfaces
     port:8000, //opcional es el puerto al cual se conectara o usar
-    strictPort: false,
-    // permite los hosts que vienen de Cloudflare Tunnel
-    allowedHosts: [CLOUDFLARE_TUNNEL_HOST, 'localhost', '127.0.0.1'],
   },
   resolve:{
     alias:{
